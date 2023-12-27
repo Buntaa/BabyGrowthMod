dde32d26c3d95db9c32f7eacfe776a6441048d8b
Buntaa <buckx420@gmail.com>
2023-12-27 05:06:51 -0500

    Add project files.

---
+++ BabyGrowthSettings.cs
@@ -0,0 +1,35 @@
+﻿using MCM.Abstractions.Attributes;
+using MCM.Abstractions.Attributes.v2;
+using MCM.Abstractions.Base.Global;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BabyGrowthMod
+{
+   public class BabyGrowthSettings : AttributeGlobalSettings<BabyGrowthSettings>
+    {
+        [SettingPropertyFloatingInteger("Growth Rate", 1f, 40f, "0.0", Order = 0, RequireRestart = false, HintText = "Adjusts the rate at which all children grow.")]
+        [SettingPropertyGroup("Baby Growth Settings")]
+        public float NewGrowthRate { get; set; } = 15f;
+
+
+        [SettingPropertyBool("Affect Only Players Children", Order = 1, RequireRestart = false, HintText = "When enabled, the mod will only affect the growth rate of the player's children.")]
+        [SettingPropertyGroup("Baby Growth Settings")]
+        public bool AffectOnlyPlayersChildren { get; set; } = false;
+
+        [SettingPropertyBool("Super Speed", Order = 2, RequireRestart = false, HintText ="When enabled, the mod will make your children grow SUPER fast. Good for people who wanna do quick runs or for fun.")]
+        [SettingPropertyGroup("Baby Growth Settings")]
+        public bool SuperSpeed { get; set; } = false;
+
+        
+
+        public override string Id => "BabyGrowthMod_v1.0.4";
+
+        public override string DisplayName => "Baby Growth Mod";
+
+        public override string FolderName => "BabyGrowthMod";
+
+        public override string FormatType => "xml";
+    }
+}
---
+++ BabyGrowthSubModule.cs
@@ -0,0 +1,73 @@
+﻿using MCM.Abstractions.Base.Global;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace BabyGrowthMod
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            base.OnGameStart(game, gameStarterObject);
+
+            if (game.GameType is Campaign)
+            {
+                CampaignEvents.DailyTickEvent.AddNonSerializedListener(this, OnDailyTick);
+            }
+        }
+        private void OnDailyTick()
+        {
+            if (GlobalSettings<BabyGrowthSettings>.Instance.AffectOnlyPlayersChildren)
+            {
+                ApplyGrowthRateToPlayerChildren();
+            }
+
+            else
+            {
+                ApplyGrowthRatesToAllChildren();
+            }
+
+            if (GlobalSettings<BabyGrowthSettings>.Instance.SuperSpeed)
+            {
+                float newGrowthRate = GlobalSettings<BabyGrowthSettings>.Instance.NewGrowthRate;
+                foreach(Hero hero in Hero.AllAliveHeroes)
+                {
+                    if (hero.IsChild && hero.IsKnownToPlayer && hero.Clan == Hero.MainHero.Clan && hero.Age < (float)Campaign.Current.Models.AgeModel.HeroComesOfAge)
+                    {
+                        hero.SetBirthDay(hero.BirthDay - CampaignTime.Days(newGrowthRate * 100));
+                    }
+                }
+            }
+        }
+
+        private void ApplyGrowthRateToPlayerChildren()
+        {
+            float newGrowthRate = GlobalSettings<BabyGrowthSettings>.Instance.NewGrowthRate;
+            foreach (Hero hero in Hero.AllAliveHeroes)
+            {
+                if (hero.IsChild && hero.IsKnownToPlayer && hero.Clan == Hero.MainHero.Clan && hero.Age < (float)Campaign.Current.Models.AgeModel.HeroComesOfAge)
+                {
+                    hero.SetBirthDay(hero.BirthDay - CampaignTime.Days(newGrowthRate + 1f));
+                }
+            }
+
+        }
+
+
+        private void ApplyGrowthRatesToAllChildren()
+        {
+            float newGrowthRate = GlobalSettings<BabyGrowthSettings>.Instance.NewGrowthRate;
+            foreach (Hero hero in Hero.AllAliveHeroes)
+            {
+                if (hero.IsChild && hero.Age < (float)Campaign.Current.Models.AgeModel.HeroComesOfAge)
+                {
+                    hero.SetBirthDay(hero.BirthDay - CampaignTime.Days(newGrowthRate - 1f));
+                }
+            }
+        }
+    }
+}
